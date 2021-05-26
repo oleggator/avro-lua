878c5c6fe2c8c2683320a1cc6e8c5f1064d8eebd
Oleg Utkin <o.utkin@corp.mail.ru>
2021-05-26 22:07:07 +0300

    add encoding

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::{io::Read};
 use mlua::{LuaSerdeExt, prelude::*};
 use avro_rs::Schema;
 use std::convert::TryFrom;
+use serde_json::Value;
 use std::sync::Arc;
 
 struct Avro {
@@ -19,6 +20,11 @@ impl Avro {
         let avro_value = avro_rs::from_avro_datum(&self.schema, reader, None)?;
         Ok(Value::try_from(avro_value)?)
     }
+
+    pub fn encode(&self, value: Value) -> Result<Vec<u8>, avro_rs::Error> {
+        let avro_value = avro_rs::types::Value::from(value);
+        avro_rs::to_avro_datum(&self.schema, avro_value)
+    }
 }
 
 impl mlua::UserData for Avro {
@@ -27,6 +33,13 @@ impl mlua::UserData for Avro {
             let json_value = this.decode(&mut blob.as_bytes())
                 .map_err(|err| mlua::Error::ExternalError(Arc::new(err)))?;
             lua.to_value(&json_value)
+        });
+
+        methods.add_method("encode", |lua, this: &Avro, table: LuaValue| {
+            let json_value: Value = lua.from_value(table)?;
+            let blob = this.encode(json_value)
+                .map_err(|err| mlua::Error::ExternalError(Arc::new(err)))?;
+            lua.create_string(&blob)
         });
     }
 }
