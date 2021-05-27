ab37eb44798e6034b90dd5b6b709ba07a2b28300
Oleg Utkin <o.utkin@corp.mail.ru>
2021-05-27 13:51:27 +0300

    add lua methods

--- src/lib.rs
+++ src/lib.rs
@@ -21,26 +21,29 @@ impl Avro {
         Ok(Value::try_from(avro_value)?)
     }
 
+    fn ldecode<'lua>(lua: &'lua Lua, this: &Avro, blob: LuaString) -> mlua::Result<mlua::Value<'lua>> {
+        let json_value = this.decode(&mut blob.as_bytes())
+            .map_err(|err| mlua::Error::ExternalError(Arc::new(err)))?;
+        lua.to_value(&json_value)
+    }
+
     pub fn encode(&self, value: Value) -> Result<Vec<u8>, avro_rs::Error> {
         let avro_value = avro_rs::types::Value::from(value);
         avro_rs::to_avro_datum(&self.schema, avro_value)
+    }
+
+    fn lencode<'lua>(lua: &'lua Lua, this: &Avro, table: LuaValue) -> mlua::Result<mlua::String<'lua>> {
+        let json_value: Value = lua.from_value(table)?;
+        let blob = this.encode(json_value)
+            .map_err(|err| mlua::Error::ExternalError(Arc::new(err)))?;
+        lua.create_string(&blob)
     }
 }
 
 impl mlua::UserData for Avro {
     fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_method("decode", |lua, this: &Avro, blob: LuaString| {
-            let json_value = this.decode(&mut blob.as_bytes())
-                .map_err(|err| mlua::Error::ExternalError(Arc::new(err)))?;
-            lua.to_value(&json_value)
-        });
-
-        methods.add_method("encode", |lua, this: &Avro, table: LuaValue| {
-            let json_value: Value = lua.from_value(table)?;
-            let blob = this.encode(json_value)
-                .map_err(|err| mlua::Error::ExternalError(Arc::new(err)))?;
-            lua.create_string(&blob)
-        });
+        methods.add_method("decode", Self::ldecode);
+        methods.add_method("encode", Self::lencode);
     }
 }
 
